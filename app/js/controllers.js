ee63e48e2517a351a3844c04dcf44f26264c6993
marcus <marcus@SFOC02NFCKHG3QD.local>
2015-02-04 22:28:35 -0800

    added some basic filter and order binding

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,23 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
     $scope.phones = [
-        {'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.'}
+        {
+            'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.',
+            'age':5
+        },
+        {
+            'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.',
+            'age': 10
+        },
+        {
+            'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.',
+            'age':7
+        }
 
     ];
+   $scope.orderProp = "age";
 });
 
