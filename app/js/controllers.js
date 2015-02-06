8e20ff8bdc2c327b9ecb15e31518c6f9f161daf1
marcus <marcus@SFOC02NFCKHG3QD.local>
2015-02-05 18:03:34 -0800

    added routing details view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,5 @@
 
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatController = angular.module('phonecatController', []);
 
 /** One method of preventing depreciation of services during minification issues using $inject**/
 //function PhoneListCtrl($scope, $http){
@@ -9,15 +9,22 @@ var phonecatApp = angular.module('phonecatApp', []);
 //    $scope.orderProp = 'age';
 //}
 //PhoneListCtrl.$inject = ['$scope', '$http'];
-//phonecatApp.controller('PhoneListCtrl', PhoneListCtrl);
+//phonecatController.controller('PhoneListCtrl', PhoneListCtrl);
+
+
 
 
 /** another means of preventing depreciation of services during minification issues**/
-
-phonecatApp.controller('PhoneListCtrl',['$scope', '$http', function($scope, $http) {
+phonecatController.controller('PhoneListCtrl',['$scope', '$http', function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
         $scope.phones = data;
     });
     $scope.orderProp = "age";
+}]);
+
+phonecatController.controller('PhoneDetailCtrl', ['$scope','$routeParams', '$http', function($scope, $http, $routeParams){
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data){
+        $scope.phone = data;
+    });
 }]);
 
